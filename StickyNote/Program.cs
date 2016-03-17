1277d2876d5870f5dd27ab07a0494eb5bf43760b
modybick <haruki.mjp@gmail.com>
2016-03-18 00:34:58 +0900

    1.0.0.2 二重起動の禁止

--- StickyNote/Program.cs
+++ StickyNote/Program.cs
@@ -14,9 +14,22 @@ namespace StickyNote
         [STAThread]
         static void Main()
         {
+            //二重起動を禁止する
+            //Mutexクラスの作成
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, "StickyNote");
+            //mutexの所有権を要求する
+            if (mutex.WaitOne(0, false) == false)
+            {   //既に起動していると判断して終了
+                MessageBox.Show("既に起動しています。","StickyNote");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            //mutexを解放する
+            mutex.ReleaseMutex();
         }
     }
 }
